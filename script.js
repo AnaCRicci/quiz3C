5ea74ada492b97349e616019b8e953f015290d22
Ana Clara <162575150+AnaCRicci@users.noreply.github.com>
2025-10-02 12:10:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = {
+    {
+        question: "Qual é o maior animal do mundo?"
+        answers: [
+            { id: 1, text:"Tubarão", correct: false}
+            { id: 2, text:"Baleia Azul", correct: true}
+            { id: 3, text:"Elefante", correct: false}
+            { id: 4, text:"Girafa", correct: false}
+        ],
+    },
+    {
+        question: "Qual o maior país do mundo?"
+        answers: [
+            { id: 1, text:"Rússia", correct: true}
+            { id: 2, text:"Brasil", correct: false}
+            { id: 3, text:"China", correct: false}
+            { id: 4, text:"EUA", correct: false}
+        ],
+    },
+    {
+        question: "Qual o menor país do mundo?"
+        answers: [
+            { id: 1, text:"Nepal", correct: false}
+            { id: 2, text:"Butão", correct: false}
+            { id: 3, text:"Japão", correct: false}
+            { id: 4, text:"Vaticano", correct: true}
+        ],
+    },
+    {
+        question: "Qual é ?"
+        answers: [
+            { id: 1, text:"Tubarão", correct: false}
+            { id: 2, text:"Baleia Azul", correct: true}
+            { id: 3, text:"Elefante", correct: false}
+            { id: 4, text:"Girafa", correct: false}
+        ],
+    },
+}
