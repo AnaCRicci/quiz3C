a45320e2e94c1a427a0ef3948dc243264bf3d746
Ana Clara <162575150+AnaCRicci@users.noreply.github.com>
2025-10-08 07:50:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const questions = {
             { id: 2, text:"Terra", correct: false},
             { id: 3, text:"Mercúrio", correct: true},
             { id: 4, text:"Marte", correct: false},
-        ],
-    },
+        ];
+    }
     {
         question; "Quaem pintou Mona Lisa?",
         answers; [
@@ -15,8 +15,8 @@ const questions = {
             { id: 2, text:"Vincent Van Gogh", correct: false},
             { id: 3, text:"Pablo Picasso", correct: false},
             { id: 4, text:"Claude Monet", correct: false},
-        ],
-    },
+        ]
+    }
     {
         question; "Qual o menor país do mundo?",
         answers; [
@@ -24,17 +24,17 @@ const questions = {
             { id: 2, text:"Butão", correct: false},
             { id: 3, text:"Japão", correct: false},
             { id: 4, text:"Vaticano", correct: true},
-        ],
-    },
+        ]
+    }
     {
         question; "Qual a capital do Brasil?",
         answers; [
             { id: 1, text:"Rio de aneiro", correct: false},
             { id: 2, text:"Brasília", correct: true},
             { id: 3, text:"São Paulo", correct: false},
-            { id: 4, text:"Salvador", correct: false},
-        ],
-    },
+            { id: 4, text:"Salvador", correct: false}
+        ]
+    }
 }
 
 const questionElement = document.getElementById("question");
@@ -51,3 +51,41 @@ function startQuiz() {
     showQuestion();
 }
 
+function resetState() {
+    nextButton.style.display = "none";
+    while (answersButtons.firstChild) {
+        answersButtons.removeChild(answersButtons.firstChild);
+    }
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(element => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.dataset.id = answer.id;
+        button.classList.add("btn");
+        button.addEventListener("click", selectAnswer);
+        answersButtons.appendChild(button);
+    });
+}
+
+function selectAnswer(e) {
+    answers = questions[currentQuestionIndex].answers;
+    const correctAnswer = answers.filter((answer) => answer.correct == true)[0];
+    
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.id == correctAnswer.id;
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else{
+        
+    }
+}   
+
+startQuiz()
